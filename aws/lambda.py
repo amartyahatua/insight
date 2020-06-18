4b320426b959d872e0be0521726750c4238d90c5
amartyahatua <amartyahatua@gmail.com>
2020-06-18 00:26:06 -0500

    lambda_api

--- aws/lambda.py
+++ aws/lambda.py
@@ -1,51 +1,46 @@
 import os
+import io
 import boto3
-import pickle
-import numpy as np
 import json
-
+import pickle
+import numpy
 
 # grab environment variables
-ENDPOINT_NAME = 'sagemaker-scikit-learn-2020-06-16-04-19-43-119'
 
-s3 = boto3.client('s3')
+
+
 bucket = 'sagemaker-us-east-2-359780414608'
 key = 'myfile.pkl'
+
+s3 = boto3.client('s3')
 object = s3.get_object(Bucket=bucket, Key=key)
+
 body_string = object['Body'].read()
 myList = pickle.loads(body_string)
 r = len(myList)
 
+ENDPOINT_NAME = 'sagemaker-scikit-learn-2020-06-16-04-19-43-119'
+runtime= boto3.client('runtime.sagemaker')
+
+
+
+
 
 def lambda_handler(event, context):
-    
+
     data = json.loads(json.dumps(event))
     payload = data['data']
     print(payload)
     objectList = payload[0]
-    test_data_new = np.zeros((1, 500))
+    print(objectList)
     result = []
     
-    for i in range(len(objectList)):
-        temp = objectList[i]
-        temp = temp.split(" ")
-            #print(temp)
-        for j in range(len(temp)):
-            x = temp[j]
-            if(len(x) > 0):
-                x = x.lower()
-                print(x)
-                p = myList[x]
-                #print(p)
+    p = myList[objectList[0]]
+    p = p.tolist()
+    for i in range(400):
+        p.append(0)
 
-                test_data_new[0][(j*100):((j+1)*100)] = p
-                
-        response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,Body=json.dumps([test_data_new]))
-        
-        result.append(self.knn.predict(test_data_new))
-    print("Class --> ",result[0])
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME, Body=json.dumps([p]))
+    result = json.loads(response['Body'].read().decode())
     
-    
-    print(r)
-    print(myList[payload[0][0]])
-    print("=============yes==@@@@@@@@@@=============")
+    return result[0]
